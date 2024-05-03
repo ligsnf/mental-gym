29a778d4bd52638c4cb1010b5ab73c617d98e8da
Josh <j.p.lu@outlook.com>
2024-05-03 21:45:27 +1000

    feat: add task card

---
+++ components/task-card.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import { useState } from 'react';
+import { Input } from './ui/input';
+import { Slider } from './ui/slider';
+
+const TaskCard = () => {
+  const [progress, setProgress] = useState<number[]>([]);
+
+  return (
+    <div className="p-4 border rounded-sm">
+      <div className="flex flex-col gap-4">
+        <div className="flex gap-2">
+          <Input placeholder="New task ..." />
+          <Input
+            className="max-w-16"
+            type="number"
+            value={progress.length > 0 ? progress[0] : ''}
+            onChange={(e) => setProgress([Number(e.target.value)])}
+          />
+        </div>
+        <Slider
+          onValueChange={(percent) => setProgress(percent)}
+          value={progress.length > 0 ? progress : undefined}
+          max={100}
+          step={1}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default TaskCard;
