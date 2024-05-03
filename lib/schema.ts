77650b0e91d478c93572fc80f45b2da04ee32a88
ligsnf <lwan0141@student.monash.edu>
2024-05-04 00:04:55 +1000

    feat: add db scripts

--- lib/schema.ts
+++ lib/schema.ts
@@ -1,4 +1,5 @@
 import { 
+  integer,
   pgTable, 
   serial, 
   varchar,
@@ -8,4 +9,5 @@ export const users = pgTable('User', {
   id: serial('id').primaryKey(),
   email: varchar('email', { length: 64 }),
   password: varchar('password', { length: 64 }),
+  total_time_spent: integer('total_time_spent').default(0),
 });
