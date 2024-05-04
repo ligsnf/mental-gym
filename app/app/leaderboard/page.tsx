d53faba6f52b88698dc4201f89a3f732fc257f80
Liangdi <lwan0141@student.monash.edu>
2024-05-04 23:09:03 +1000

    feat: style leaderboard

--- app/app/leaderboard/page.tsx
+++ app/app/leaderboard/page.tsx
@@ -1,12 +1,37 @@
-import TableDemo from "@/components/leaderboard";
-import { Table } from "@/components/ui/table";
+import LeaderboardTable from "@/components/leaderboard";
+import { ScrollArea } from "@/components/ui/scroll-area"
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
 
 const LeaderboardPage = () => {
   return (
     <div className="m-auto max-w-xl">
-      <div className="flex flex-col gap-4">
-        <h2 className="text-4xl font-medium">Leaderboard</h2>
-        <TableDemo />
+      <div className="flex flex-col gap-6">
+        <div className="flex justify-between">
+          <h2 className="text-4xl font-medium">Leaderboard</h2>
+          <Select>
+            <SelectTrigger className="w-[180px]">
+              <SelectValue placeholder="Global" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectItem value="global">Global</SelectItem>
+                <SelectItem value="university">University</SelectItem>
+                <SelectItem value="friends">Friends</SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+        </div>
+        <ScrollArea className="h-[42rem] rounded-md border">
+          <LeaderboardTable />
+        </ScrollArea>
       </div>
     </div>
   );
