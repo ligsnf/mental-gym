77a82b8eb9ea49f2695b8d73edcf4a19d904bc78
Kanishka <kanishkawickramasinghe50@gmail.com>
2024-05-04 18:46:24 +1000

    c

--- components/leaderboard.tsx
+++ components/leaderboard.tsx
@@ -16,27 +16,28 @@ import { Button } from "@/components/ui/button"
 export async function TableDemo() {
   const users = await getUsers(); 
 
+  // Only select the first 10 users to display
+  const topTenUsers = users.slice(0, 20);
+
+
   console.log(users)
   return (
-    <div className="w-full max-w-[500px] mx-auto overflow-hidden">
+    <div className="w-full max-w-[500px] mx-auto overflow-hidden ">
       <Table className="w-full table-fixed text-lg">
         <TableHeader>
           <TableRow>
             <TableHead className="w-1/2 p-2 text-left">Username</TableHead>
-            <TableHead className="w-1/2 p-2 text-right">Points</TableHead>
+            <TableHead className="w-1/2 p-2 text-right">Experience</TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
-            {users.map((user, index) => (
+            {topTenUsers.map((user, index) => (
             <TableRow key={index}>
               <TableCell className="p-2 text-left font-medium">{user.email}</TableCell>
             </TableRow>
           ))}
         </TableBody>
-        <TableFooter>
-            <Button variant="outline" size="icon" >
-              <ChevronRight className="h-4 w-4 text-right" />
-            </Button>
+        <TableFooter>          
         </TableFooter>
       </Table>
     </div>
