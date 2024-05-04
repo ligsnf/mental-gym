f2384307e072bd9ecbbf03c9e0676facfe6b856a
Liangdi <lwan0141@student.monash.edu>
2024-05-04 19:55:56 +1000

    feat: add profile page with tremo.so charts

--- app/app/profile/page.tsx
+++ app/app/profile/page.tsx
@@ -1,14 +1,153 @@
 import { auth, signOut } from 'app/auth';
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "@/components/ui/avatar"
+import { Button } from "@/components/ui/button"
+import { Card } from '@tremor/react';
+import { ProgressChart } from './-components/chart';
+import { StreakTracker } from './-components/tracker';
+
+const trackerData = [
+  { color: 'emerald', tooltip: 'May 2' },
+  { color: 'rose', tooltip: 'May 3' },
+  { color: 'emerald', tooltip: 'May 4' },
+  { color: 'emerald', tooltip: 'May 5' },
+  { color: 'emerald', tooltip: 'May 6' },
+  { color: 'emerald', tooltip: 'May 7' },
+  { color: 'emerald', tooltip: 'May 8' },
+  { color: 'emerald', tooltip: 'May 9' },
+  { color: 'emerald', tooltip: 'May 10' },
+  { color: 'rose', tooltip: 'May 11' },
+  { color: 'emerald', tooltip: 'May 12' },
+  { color: 'rose', tooltip: 'May 13' },
+  { color: 'emerald', tooltip: 'May 14' },
+  { color: 'emerald', tooltip: 'May 15' },
+  { color: 'emerald', tooltip: 'May 16' },
+  { color: 'emerald', tooltip: 'May 17' },
+  { color: 'rose', tooltip: 'May 18' },
+  { color: 'emerald', tooltip: 'May 19' },
+  { color: 'emerald', tooltip: 'May 20' },
+  { color: 'emerald', tooltip: 'May 21' },
+  { color: 'emerald', tooltip: 'May 22' },
+  { color: 'emerald', tooltip: 'May 23' },
+  { color: 'emerald', tooltip: 'May 24' },
+  { color: 'emerald', tooltip: 'May 25' },
+  { color: 'emerald', tooltip: 'May 26' },
+  { color: 'emerald', tooltip: 'May 27' },
+  { color: 'rose', tooltip: 'May 28' },
+  { color: 'emerald', tooltip: 'May 29' },
+  { color: 'emerald', tooltip: 'May 30' },
+  { color: 'rose', tooltip: 'May 31' },
+  { color: 'emerald', tooltip: 'Jun 1' },
+  { color: 'emerald', tooltip: 'Jun 2' },
+  { color: 'emerald', tooltip: 'Jun 3' },
+  { color: 'emerald', tooltip: 'Jun 4' },
+  { color: 'emerald', tooltip: 'Jun 5' },
+  { color: 'emerald', tooltip: 'Jun 6' },
+  { color: 'emerald', tooltip: 'Jun 7' },
+  { color: 'emerald', tooltip: 'Jun 8' },
+  { color: 'emerald', tooltip: 'Jun 9' },
+  { color: 'rose', tooltip: 'Jun 10' },
+  { color: 'emerald', tooltip: 'Jun 11' },
+  { color: 'rose', tooltip: 'Jun 12' },
+  { color: 'emerald', tooltip: 'Jun 13' },
+  { color: 'emerald', tooltip: 'Jun 14' },
+  { color: 'emerald', tooltip: 'Jun 15' },
+  { color: 'emerald', tooltip: 'Jun 16' },
+  { color: 'rose', tooltip: 'Jun 17' },
+];
+
+const chartdata = [
+  {
+    date: 'Jan 22',
+    'XP earned': 2338,
+  },
+  {
+    date: 'Feb 22',
+    'XP earned': 2103,
+  },
+  {
+    date: 'Mar 22',
+    'XP earned': 2194,
+  },
+  {
+    date: 'Apr 22',
+    'XP earned': 2108,
+  },
+  {
+    date: 'May 22',
+    'XP earned': 1812,
+  },
+  {
+    date: 'Jun 22',
+    'XP earned': 1726,
+  },
+  {
+    date: 'Jul 22',
+    'XP earned': 1982,
+  },
+  {
+    date: 'Aug 22',
+    'XP earned': 2012,
+  },
+  {
+    date: 'Sep 22',
+    'XP earned': 2342,
+  },
+  {
+    date: 'Oct 22',
+    'XP earned': 2473,
+  },
+  {
+    date: 'Nov 22',
+    'XP earned': 3848,
+  },
+  {
+    date: 'Dec 22',
+    'XP earned': 3736,
+  },
+];
 
 export default async function ProtectedPage() {
   let session = await auth();
+  let username = session?.user?.email;
+
+  if (!session?.user) {
+    return (
+      <div>
+        <p>You need to be signed in to access this page</p>
+      </div>
+    );
+  }
 
   return (
-    <div className="flex h-screen">
-      <div className="w-screen h-screen flex flex-col space-y-5 justify-center items-center">
-        You are logged in as {session?.user?.email}
-        <SignOut />
+    <div className="flex flex-col gap-8">
+      <div className="flex items-center gap-4">
+        <Avatar className="hidden h-9 w-9 sm:flex border-2 border-slate-900 dark:border-white">
+          <AvatarImage src="https://www.gravatar.com/avatar/?d=identicon" alt="Avatar" />
+          <AvatarFallback>{username?.substring(0, 2).toUpperCase()}</AvatarFallback>
+        </Avatar>
+        <div className="grid gap-1">
+          <p className="text-sm font-medium leading-none">
+            {username}
+          </p>
+          <p className="text-sm text-muted-foreground">
+            {username}@gmail.com
+          </p>
+        </div>
+        <div className="ml-auto font-medium">
+          <SignOut />
+        </div>
       </div>
+      <Card className="mx-auto">
+        <StreakTracker data={trackerData} />
+      </Card>
+      <h2 className="text-3xl font-medium">XP progress</h2>
+      <Card>
+        <ProgressChart chartdata={chartdata} valueKey='XP earned' />
+      </Card>
     </div>
   );
 }
@@ -21,7 +160,7 @@ function SignOut() {
         await signOut();
       }}
     >
-      <button type="submit">Sign out</button>
+      <Button type="submit">Sign out</Button>
     </form>
   );
 }
