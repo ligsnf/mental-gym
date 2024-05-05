c31007c784531da922d0c0c386db1d0c0005aedf
Josh <j.p.lu@outlook.com>
2024-05-05 12:18:04 +1000

    fix: lock in component

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,14 +1,11 @@
 import LockIn from '@/components/lock-in';
 import TaskList from '@/components/task-list';
-import { Card } from '@tremor/react';
 
 const AppPage = () => {
   return (
-    <div className="flex flex-col gap-6">
+    <div className="m-auto max-w-xl flex flex-col gap-6">
       <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
-      <Card>
-        <LockIn />
-      </Card>
+      <LockIn />
       <div className="w-[36rem] space-y-2">
         <h2 className="text-3xl font-bold tracking-tight">Tasks</h2>
         <TaskList />
