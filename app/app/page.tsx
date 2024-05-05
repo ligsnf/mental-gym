c2b5329955a23a18c967e1d437f1be84c8bf33b5
Josh <j.p.lu@outlook.com>
2024-05-05 11:46:11 +1000

    style: adjust headings

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -4,7 +4,7 @@ const AppPage = () => {
   return (
     <div className="m-auto max-w-xl">
       <div className="flex flex-col gap-4">
-        <h2 className="text-4xl font-medium">Tasks</h2>
+        <h2 className="text-3xl font-bold tracking-tight">Tasks</h2>
         <TaskList />
       </div>
     </div>
